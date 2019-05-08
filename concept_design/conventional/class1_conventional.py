d722728457b0c9a55f63ed5a46fb9d867c5898cc
Rick Nelen <ricknelen@gmail.com>
2019-05-08 11:34:09 +0200

    finished Weights definition

--- concept_design/conventional/class1_conventional.py
+++ concept_design/conventional/class1_conventional.py
@@ -3,23 +3,25 @@
 
 # THIS SECTION IS USED FOR JET AIRCRAFT; FOR TURBOPROP AIRCRAFT, SCROLL DOWN
 
-def Weights(jet = False, tbp = False)
+def Weights(W_empty_jet, W_empty_tbp, W_payload, W_crew, C_fe, S, S_wet, A_jet, A_tbp, e_jet, e_tbp, cj_loiter_jet, cj_cruise_jet, eff_loiter_tbp, eff_cruise_tbp, cp_loiter_tbp, cp_cruise_tbp, jet = False, tbp = False)
     # Calculate drag polar
     # Find C_D_0
     C_D_0 = C_fe * S_wet/S
     # Find C_D
     # Assume that e is in the range of 0.80 and 0.85 for turboprops and 0.75 - 0.85 for jets
-    C_D = C_D_0 + C_L**2 / (pi * A * e)
+    # C_D = C_D_0 + C_L**2 / (pi * A * e)
 
     if jet:
     # Determine the maximum L/D for jets
-        LD_cruise_jet = 3/4 * sqrt((pi * A * e) / (3 * C_D_0))
+        LD_cruise_jet = 3/4 * sqrt((pi * A_jet * e_jet) / (3 * C_D_0))
 
         # Requirements for loiter
         # CL/CD during loiter for a jet
-        C_L_loiter_jet = sqrt(C_D_0 * pi * A * e)
+        C_L_loiter_jet = sqrt(C_D_0 * pi * A_jet * e_jet)
         C_D_loiter_jet = 2 * C_D_0
         LD_loiter_jet = C_L_loiter_jet / C_D_loiter_jet
+
+        f_fuel_jet = fuel_fraction(LD_cruise_jet = LD_cruise_jet, cj_cruise_jet = cj_cruise_jet, cj_loiter_jet = cj_loiter_jet, LD_loiter_jet = LD_loiter_jet, jet = True)
 
         # Formula for the maximum take-off weight
         MTOW_jet = (W_empty_jet + W_crew + W_payload) / (1 - f_trapped_fuel + f_used_fuel_jet * (1 + f_reserve_fuel))
@@ -41,11 +43,39 @@ def Weights(jet = False, tbp = False)
         # Determine the operative empty weight for a jet
         OEW_jet = W_empty_jet + W_trapped_fuel_jet + W_crew
 
+        return MTOW_jet, OEW_jet, W_fuel_jet
+
     if tbp:
+    # Determine the maximum L/D for a turboprop
+        LD_cruise_tbp = sqrt((pi * A_tbp * e_tbp) / (4 * C_D_0))
 
-W_trapped_fuel_tbp = fraction_trapped_fuel * MTOW_tbp
+        # Requirements for loiter
+        # CL/CD during loiter for a turboprop
+        C_L_loiter_tbp = sqrt(3 * C_D_0 * pi * A_tbp * e_tbp)
+        C_D_loiter_tbp = 4 * C_D_0
+        LD_loiter_tbp = C_L_loiter_tbp / C_D_loiter_tbp
 
-# Determine take-off weight for a turboprop
-MTOW_tbp = OEW_tbp + W_fuel_tbp + W_payload
-# Determine the operative empty weight for a turboprop
-OEW_tbp = W_empty_tbp + W_trapped_fuel_tbp + W_crew
+        # Determine fuel fractions for a turboprop
+        f_fuel_tbp = fuel_fraction(LD_cruise_tbp = LD_cruise_tbp, eff_cruise_tbp = eff_cruise_tbp, eff_loiter_tbp = eff_loiter_tbp, cp_cruise_tbp = cp_cruise_tbp, cp_loiter_tbp = cp_loiter_tbp, tbp = True)
+
+        # Formula for the maximum take-off weight
+        MTOW_tbp = (W_empty_tbp + W_crew + W_payload) / (1 - f_trapped_fuel + f_used_fuel_tbp * (1 + f_reserve_fuel))
+
+        # Trapped fuel
+        W_trapped_fuel_tbp = fraction_trapped_fuel * MTOW_tbp
+
+        # Useful fuel
+        # Used fuel
+        f_used_fuel_tbp = 1 - f_fuel_tbp
+        W_used_fuel_tbp = f_used_fuel_tbp * MTOW_tbp
+        # Reserve fuel
+        W_reserve_fuel_tbp = f_reserve_fuel * W_used_fuel_tbp
+        # Total useful fuel
+        W_fuel_tbp = W_used_fuel_tbp + W_reserve_fuel_tbp
+
+        # Determine take-off weight for a turboprop
+        # MTOW_tbp = OEW_tbp + W_fuel_tbp + W_payload
+        # Determine the operative empty weight for a turboprop
+        OEW_tbp = W_empty_tbp + W_trapped_fuel_tbp + W_crew
+
+        return MTOW_tbp, OEW_tbp, W_fuel_tbp
