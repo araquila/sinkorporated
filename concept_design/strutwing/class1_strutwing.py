264b1238e989f1dbd55dfda54d25983675e1fc6d
Toon Stolk <toonstolk@gmail.com>
2019-05-08 16:34:31 +0200

    brushed up some strutwing files

--- concept_design/strutwing/class1_strutwing.py
+++ concept_design/strutwing/class1_strutwing.py
@@ -1,34 +1,54 @@
-import numpy as np
-import fuel_fraction as ff
+# CLASS 1 ESTIMATION OF THE STRUTTED AIRCRAFT SIZING PROGRAM
 
-# INPUT VARIABLES
-W_payload = 6120. # [kg]
-A = 19.5 # [-]
-e = 0.8 # [-]
-C_fe = 0.0030 # [-]
-S_wet = 5.5 # [m^2]
-S = 1. # [m^2]
-C_D_0 = (S_wet/S) * C_fe
-LD_cruise_tbp = np.sqrt((np.pi*A*e)/(4*C_D_0))
-LD_cruise_jet = (3/4) * np.sqrt((np.pi*A*e)/(3*C_D_0))
+# Import modules
+from class1_conventional import Weights
 
+# Gravitional constant
+g = 9.8065
 
-# BASED ON STATISTICS: CALCULATION OF THE OEW AND MTOW_jet
-payload_ratio_tbp = 0.22 # = PL/MTOW
-emptyweight_ratio_tbp = 0.615 # = OEW/MTOW
-payload_ratio_jet = 0.22 # = PL/MTOW
-emptyweight_ratio_jet = 0.570 # = OEW/MTOW
-MTOW_tbp = W_payload / payload_ratio_tbp
-OEW_tbp = MTOW_tbp * emptyweight_ratio_tbp
-MTOW_jet = W_payload / payload_ratio_jet
-OEW_jet = W_payload * emptyweight_ratio_jet
+# Passengers and crew
+n_passenger = 60
+M_passenger = 102 #(including luggage)
+n_crew = 4
+M_crew_member = 100
 
+# Initial mass and fractions
+M_payload = n_passenger * M_passenger
+M_crew = n_crew * M_crew_member
+f_trapped_fuel = 0.003      # Range 0.001-0.005
+M_empty_tbp = 13000
+M_empty_jet = 10000
 
-# CALCULATE FUEL WEIGHT
-ff_tbp = ff.fuel_fraction(LD_cruise_tbp = LD_cruise_tbp, tbp = True)
-ff_jet = ff.fuel_fraction(LD_cruise_jet = LD_cruise_jet, jet = True)
-W_fuel_tbp = (1-ff_tbp)*MTOW_tbp
-W_fuel_jet = (1-ff_jet)*MTOW_jet
+# Convert to weights
+W_payload = M_payload * g
+W_crew = M_crew * g
+W_empty_tbp = M_empty_tbp * g
+W_empty_jet = M_empty_jet * g
 
-print(MTOW_tbp)
-print(W_payload + W_fuel_tbp + OEW_tbp)
+## Initial jet and tbp aircraft parameters
+C_fe = 0.003
+S = 1
+S_wet = 5 * S
+
+# Jet
+A_jet = 19.5
+e_jet = 0.85
+cj_loiter_jet = 0.5         # (0.4-0.6) [lbs/lbs/hr]
+cj_cruise_jet = 0.6         # (0.5-0.9) [lbs/lbs/hr]
+
+# Tbp
+A_tbp = 19.5
+e_tbp = 0.85
+eff_cruise_tbp = 0.85       # [-]
+eff_loiter_tbp = 0.77       # [-]
+cp_cruise_tbp = 0.5         # (0.4-0.6) [lbs/hp/hr]
+cp_loiter_tbp = 0.6         # (0.5-0.7) [lbs/hp/hr]
+
+for iter in range(50):
+    MTOW_jet, OEW_jet, W_fuel_jet =  Weights(W_empty_jet, W_empty_tbp, W_payload, W_crew, C_fe, S, S_wet, A_jet, A_tbp, e_jet, e_tbp, cj_loiter_jet, cj_cruise_jet, eff_loiter_tbp, eff_cruise_tbp, cp_loiter_tbp, cp_cruise_tbp, f_trapped_fuel, jet = True)
+    MTOW_tbp, OEW_tbp, W_fuel_tbp = Weights(W_empty_jet, W_empty_tbp, W_payload, W_crew, C_fe, S, S_wet, A_jet, A_tbp, e_jet, e_tbp, cj_loiter_jet, cj_cruise_jet, eff_loiter_tbp, eff_cruise_tbp, cp_loiter_tbp, cp_cruise_tbp, f_trapped_fuel, tbp = True)
+    W_empty_jet = 0.54*MTOW_jet
+    W_empty_tbp = 0.52*MTOW_tbp
+
+print(MTOW_tbp/g, OEW_tbp/g, W_fuel_tbp/g)
+print(MTOW_jet/g, OEW_jet/g, W_fuel_jet/g)
