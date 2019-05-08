d78a260f83218de0b6444bb6f346d618cf10e181
Rick Nelen <ricknelen@gmail.com>
2019-05-08 12:11:11 +0200

    Fixed main conventions

--- concept_design/conventional/main_conventional.py
+++ concept_design/conventional/main_conventional.py
@@ -44,9 +44,9 @@ eff_loiter_tbp = 0.77       # [-]
 cp_cruise_tbp = 0.5         # (0.4-0.6) [lbs/hp/hr]
 cp_loiter_tbp = 0.6         # (0.5-0.7) [lbs/hp/hr]
 
-for iter in range(1):
-    MTOW_jet, OEW_jet, W_fuel_jet =  Weights(W_empty_jet, W_empty_tbp, W_payload, W_crew, C_fe, S, S_wet, A_jet, A_tbp, e_jet, e_tbp, cj_loiter_jet, cj_cruise_jet, eff_loiter_tbp, eff_cruise_tbp, cp_loiter_tbp, cp_cruise_tbp, f_trapped_fuel, jet = True)
-    MTOW_tbp, OEW_tbp, W_fuel_tbp = Weights(W_empty_jet, W_empty_tbp, W_payload, W_crew, C_fe, S, S_wet, A_jet, A_tbp, e_jet, e_tbp, cj_loiter_jet, cj_cruise_jet, eff_loiter_tbp, eff_cruise_tbp, cp_loiter_tbp, cp_cruise_tbp, f_trapped_fuel, tbp = True)
+for iter in range(3):
+    MTOW_jet, OEW_jet, W_fuel_jet =  Weights_Class_I(W_empty_jet, W_empty_tbp, W_payload, W_crew, C_fe, S, S_wet, A_jet, A_tbp, e_jet, e_tbp, cj_loiter_jet, cj_cruise_jet, eff_loiter_tbp, eff_cruise_tbp, cp_loiter_tbp, cp_cruise_tbp, f_trapped_fuel, jet = True)
+    MTOW_tbp, OEW_tbp, W_fuel_tbp = Weights_Class_I(W_empty_jet, W_empty_tbp, W_payload, W_crew, C_fe, S, S_wet, A_jet, A_tbp, e_jet, e_tbp, cj_loiter_jet, cj_cruise_jet, eff_loiter_tbp, eff_cruise_tbp, cp_loiter_tbp, cp_cruise_tbp, f_trapped_fuel, tbp = True)
 
-print(MTOW_tbp)
-print(MTOW_jet)
+    print(MTOW_tbp)
+    print(MTOW_jet)
