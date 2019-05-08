d2cc9f59caf3a8b7c7f3ff682b480b451d1045cc
Rick Nelen <ricknelen@gmail.com>
2019-05-08 09:24:09 +0200

    class I finished up to fuel fractions

--- concept_design/conventional/class1_conventional.py
+++ concept_design/conventional/class1_conventional.py
@@ -5,7 +5,13 @@ fraction_trapped_fuel = 0.003
 W_trapped_fuel_jet = fraction_trapped_fuel * MTOW_jet
 
 # Useful fuel
-W_fuel_jet = W_used_fuel_jet 
+# Reserve fuel
+W_reserve_fuel_jet = f_reserve_fuel * W_used_fuel_jet
+# Used fuel
+f_used_fuel_jet = 1 - f_fuel_jet
+W_used_fuel_jet = f_used_fuel_jet * MTOW_jet
+# Total useful fuel
+W_fuel_jet = W_used_fuel_jet + W_reserve_fuel_jet
 
 # Determine take-off weight for a jet
 MTOW_jet = OEW_jet + W_fuel_jet + W_payload
