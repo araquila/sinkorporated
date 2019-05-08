493bfe4508d30e1047541b540001b1223ac9dbc2
Matthijs Torsy <m.torsij@student.tudelft.nl>
2019-05-08 09:09:41 +0200

    updated versions

---
+++ concept_design/conventional/main_conventional.py
@@ -0,0 +1,14 @@
+# MAIN OF THE CONVENTIAL AIRCRAFT SIZING PROGRAM
+
+# Import modules
+import fuel_fraction
+
+# Weights
+W_payload = 10
+W_crew = 10
+MTOW_jet = 10
+MTOW_tbp = 10
+
+# Fuel fractions
+ff_jet = fuel_fraction(jet = True)
+ff_tbp = fuel_fraction(tbp = True)
