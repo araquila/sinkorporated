6be5b62c28ac21882f7fd26678fa1f4c86d09254
Bas <basvmanen@gmail.com>
2019-05-08 11:41:14 +0200

    Added Class 1 for strutwing

--- concept_design/strutwing/class1_strutwing.py
+++ concept_design/strutwing/class1_strutwing.py
@@ -1,28 +1,26 @@
+import numpy as np
+import fuel_fraction as ff
+
+# INPUT VARIABLES
 W_payload = 60000. # [N]
-W_crew = 0. # [N]
-W_trapped_fuel_jet = 0. # [N]
-W_trapped_fuel_tbp = 0. # [N]
-
-V_cruise_jet = 0. # [m/s]
-V_cruise_tbp = 0. # [m/s]
-
-eff_prop = 0. # [-]
-cp_cruise_tbp = 0. # [g/J]
-cj_cruise_jet = 0. # [g/Ns]
-
-g = 9.80665 # [m/s^2]
-
 A = 19.5 # [-]
-e = 0.75 # [-]
-C_D_0 = 0.015 # [-]
+e = 0.8 # [-]
 C_fe = 0.0030 # [-]
+S_wet = 5.5 # [m^2]
+S = 1. # [m^2]
+C_D_0 = (S_wet/S) * C_fe
+LD = np.sqrt((np.pi*A*e)/(4*C_D_0))
+print(LD)
 
 # BASED ON STATISTICS: CALCULATION OF THE OEW AND MTOW_jet
 payload_ratio = 0.22 # = PL/MTOW
 emptyweight_ratio = 0.636 # = OEW/MTOW
-
 MTOW_tbp = W_payload / payload_ratio
 OEW_tbp = MTOW_tbp * emptyweight_ratio
 
+# CALCULATE FUEL WEIGHT
+ff_tbp = ff.fuel_fraction(LD_cruise_tbp = LD, tbp = True)
+W_fuel = (1-ff_tbp)*MTOW_tbp
+
 print(MTOW_tbp)
-print(OEW_tbp)
+print(W_payload + W_fuel + OEW_tbp)
