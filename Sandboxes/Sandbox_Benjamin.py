d1a1517e34854cdae26c2d9e37129ee0d589328c
benjaminharrison <benjaminharrison1998@gmail.com>
2019-05-08 17:15:42 +0200

    update

--- Sandboxes/Sandbox_Benjamin.py
+++ Sandboxes/Sandbox_Benjamin.py
@@ -26,6 +26,10 @@ if jet==True:
     G=(T_t4/600)-1.25
 
     massflow=(T_TO_jet*(1+bypass_ratio))/(n_engines*a_0*math.sqrt(5*e_nozzle*G*(1+e_tf*bypass_ratio)))
-
+    #Intake dimensions
     inlet_spinner_ratio=0.05*(1+(rho_0*a_0)/massflow+3*bypass_ratio/(1+bypass_ratio))
     diameter_inlet=1.65*math.sqrt((massflow/(rho_0*a_0)+0.0050)/(1-(inlet_spinner_ratio)**2))
+    diameter_highlight=diameter_inlet
+    #Fan cowling dimensions
+    if jettypeB==True:
+        lenth_nacelle=c_l_nacelle*(math.sqrt((massflow*(1+0.2*bypass_ratio))/(rho_0*a_0*(1+bypass_ratio)))+delta_l_nacelle)
