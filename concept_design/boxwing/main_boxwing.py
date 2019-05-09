c13f803d344b987690e80363f7f0c80c23e34abf
Erik Peeters <ecp1@live.nl>
2019-05-09 09:15:42 +0200

    box

--- concept_design/boxwing/main_boxwing.py
+++ concept_design/boxwing/main_boxwing.py
@@ -20,8 +20,9 @@ M_MTOM = 25584.5
 M_payload = n_passenger * M_passenger
 M_crew = n_crew * M_crew_member
 f_trapped_fuel = 0.003      # Range 0.001-0.005
-M_empty_tbp = 0
+M_empty_tbp = 14000
 M_empty_jet = (M_OEM-M_crew)-0.003*M_MTOM
+
 
 # Convert to weights
 W_payload = M_payload * g
@@ -41,17 +42,21 @@ cj_loiter_jet = 0.5         # (0.4-0.6) [lbs/lbs/hr]
 cj_cruise_jet = 0.6        # (0.5-0.9) [lbs/lbs/hr]
 
 # Tbp
-A_tbp = 15
-e_tbp = 0.85
+A_tbp = 10
+e_tbp = 0.95
 eff_cruise_tbp = 0.85       # [-]
 eff_loiter_tbp = 0.77       # [-]
 cp_cruise_tbp = 0.5         # (0.4-0.6) [lbs/hp/hr]
 cp_loiter_tbp = 0.6         # (0.5-0.7) [lbs/hp/hr]
 
-for iter in range(1):
+for iter in range(5):
     # CLASS I ESTIMATION
     MTOW_jet, OEW_jet, W_fuel_jet =  Weights_Class_I(W_empty_jet, W_empty_tbp, W_payload, W_crew, C_fe, S, S_wet, A_jet, A_tbp, e_jet, e_tbp, cj_loiter_jet, cj_cruise_jet, eff_loiter_tbp, eff_cruise_tbp, cp_loiter_tbp, cp_cruise_tbp, f_trapped_fuel, jet = True)
     MTOW_tbp, OEW_tbp, W_fuel_tbp = Weights_Class_I(W_empty_jet, W_empty_tbp, W_payload, W_crew, C_fe, S, S_wet, A_jet, A_tbp, e_jet, e_tbp, cj_loiter_jet, cj_cruise_jet, eff_loiter_tbp, eff_cruise_tbp, cp_loiter_tbp, cp_cruise_tbp, f_trapped_fuel, tbp = True)
 
+    W_empty_jet = (OEW_jet-M_crew*g)-0.003*MTOW_jet
+    print(W_empty_jet)
+    #W_empty_tbp = 0.52*MTOW_tbp
+    #W_empty_jet = 0.54*MTOW_jet
 print(MTOW_tbp)
 print(MTOW_jet)
