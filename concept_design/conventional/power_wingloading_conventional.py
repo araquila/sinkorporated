fa1805d7871a94f78afa9261068d99387b07e67d
PieterBijl <36629356+PieterBijl@users.noreply.github.com>
2019-05-09 10:51:24 +0200

    Added the right symbols for CL

--- concept_design/conventional/power_wingloading_conventional.py
+++ concept_design/conventional/power_wingloading_conventional.py
@@ -45,22 +45,24 @@ W_L = 120000.
 S = 55 #[m2]
 
 #data props
-CLmax_turboprop_clean_min = 1.5
-CLmax_turboprop_clean_max = 1.9
-CLmax_turboprop_take_min = 1.7
-CLmax_turboprop_take_max = 2.1
-CLmax_turboprop_land_min = 1.9
-CLmax_turboprop_land_max = 3.3
+MTOW_tbp = 200000 #[N]
+C_L_max_tbp_clean_min = 1.5
+C_L_max_tbp_clean_max = 1.9
+C_L_max_tbp_take_min = 1.7
+C_L_max_tbp_take_max = 2.1
+C_L_max_tbp_land_min = 1.9
+C_L_max_tbp_land_max = 3.3
 
 #take off parameter turboprop
-TOP_aquila_turboprop = 580
+TOP_aquila_tbp = 580
 #data jets
-CLmax_jet_clean_min = 1.2
-CLmax_jet_clean_max = 1.8
-CLmax_jet_take_min = 1.6
-CLmax_jet_take_max = 2.2
-CLmax_jet_land_min = 1.8
-CLmax_jet_land_max = 2.8
+MTOW_jet = 230000 #[N]
+C_L_max_jet_clean_min = 1.2
+C_L_max_jet_clean_max = 1.8
+C_L_max_jet_take_min = 1.6
+C_L_max_jet_take_max = 2.2
+C_L_max_jet_land_min = 1.8
+C_L_max_jet_land_max = 2.8
 
 #take off parameter jet
 TOP_aquila_jet_single = 6000
@@ -68,18 +70,18 @@ TOP_aquile_jet_double = 6000
 
 #props, for jets scroll DOWN################
 #calculate stall speeds and the wing loading
-V_stall = V_stall_calc(W_TO,rho0,CLmax_turboprop_take_max,S)
-W_S_stall = W_S_calc(rho0,V_stall,CLmax_turboprop_take_max)
+V_stall = V_stall_calc(W_TO,rho0,C_L_max_tbp_take_max,S)
+W_S_stall = W_S_calc(rho0,V_stall,C_L_max_tbp_take_max)
 
 ##########take-off################
-k = TOP_aquila_turboprop
-CL_TO_min_turboprop = CL_TO_calc(CLmax_turboprop_take_min)
-CL_TO_max_turboprop = CL_TO_calc(CLmax_turboprop_take_max)
-CL_TO_range_turboprop = np.linspace(CL_TO_min_turboprop,CL_TO_max_turboprop,5)
-TOP_takeoff_turboprop = np.zeros(shape=(len(CL_TO_range_turboprop),len(W_S_x)))
-for i in range(len(TOP_takeoff_turboprop)):
+k = TOP_aquila_tbp
+C_L_TO_min_tbp = CL_TO_calc(C_L_max_tbp_take_min)
+C_L_TO_max_tbp = CL_TO_calc(C_L_max_tbp_take_max)
+C_L_TO_range_tbp = np.linspace(C_L_TO_min_tbp,C_L_TO_max_tbp,5)
+TOP_takeoff_tbp = np.zeros(shape=(len(C_L_TO_range_tbp),len(W_S_x)))
+for i in range(len(TOP_takeoff_tbp)):
     for j in range(len(W_S_x)):
-        TOP_takeoff_turboprop[i,j] = W_P_calc(W_S_x[j],k,CL_TO_range_turboprop[i])
+        TOP_takeoff_tbp[i,j] = W_P_calc(W_S_x[j],k,C_L_TO_range_tbp[i])
 
 # the data
 l = np.linspace(0, 0.8, 200)
@@ -92,15 +94,15 @@ xlim = 4000
 ylim = 1
 
 # plot lines
-ax1.plot(W_S_x,TOP_takeoff_turboprop[0,:])
-ax1.plot(W_S_x,TOP_takeoff_turboprop[1,:])
-ax1.plot(W_S_x,TOP_takeoff_turboprop[2,:])
-ax1.plot(W_S_x,TOP_takeoff_turboprop[3,:])
-ax1.plot(W_S_x,TOP_takeoff_turboprop[4,:])
+ax1.plot(W_S_x,TOP_takeoff_tbp[0,:])
+ax1.plot(W_S_x,TOP_takeoff_tbp[1,:])
+ax1.plot(W_S_x,TOP_takeoff_tbp[2,:])
+ax1.plot(W_S_x,TOP_takeoff_tbp[3,:])
+ax1.plot(W_S_x,TOP_takeoff_tbp[4,:])
 ax1.axvline(vertical)
 
 # plot filled parts of the graph
-plotfiller(ax1, xlim, ylim, x_data = W_S_x, data = TOP_takeoff_turboprop[4,:], direction = "up")
+plotfiller(ax1, xlim, ylim, x_data = W_S_x, data = TOP_takeoff_tbp[4,:], direction = "up")
 plotfiller(ax1, xlim, ylim, vline = vertical, direction = "right")
 
 # plot cosmetics (add some legends/labels/title)
@@ -112,23 +114,23 @@ plt.show()
 
 ##########landing#################
 f = W_L/W_TO
-W_S_landing = W_S_landing_calc(CLmax_turboprop_land_max,rho,V_landing,f)
+W_S_landing = W_S_landing_calc(C_L_max_tbp_land_max,rho,V_landing,f)
 print(W_S_landing)
 
 ########jets################
 #calculate stall speeds and the wing loading
-V_stall = V_stall_calc(W_TO,rho0,CLmax_jet_take_max,S)
-W_S_stall = W_S_calc(rho0,V_stall,CLmax_jet_take_max)
+V_stall = V_stall_calc(W_TO,rho0,C_L_max_jet_take_max,S)
+W_S_stall = W_S_calc(rho0,V_stall,C_L_max_jet_take_max)
 
 ##########take-off################
 k = TOP_aquila_jet_single
-CL_TO_min_jet = CL_TO_calc(CLmax_jet_take_min)
-CL_TO_max_jet = CL_TO_calc(CLmax_jet_take_max)
-CL_TO_range_jet = np.linspace(CL_TO_min_jet,CL_TO_max_jet,5)
-TOP_takeoff_jet = np.zeros(shape=(len(CL_TO_range_jet),len(W_S_x)))
+C_L_TO_min_jet = CL_TO_calc(C_L_max_jet_take_min)
+C_L_TO_max_jet = CL_TO_calc(C_L_max_jet_take_max)
+C_L_TO_range_jet = np.linspace(C_L_TO_min_jet,C_L_TO_max_jet,5)
+TOP_takeoff_jet = np.zeros(shape=(len(C_L_TO_range_jet),len(W_S_x)))
 for i in range(len(TOP_takeoff_jet)):
     for j in range(len(W_S_x)):
-        TOP_takeoff_jet[i,j] = T_W_calc(W_S_x[j],k,CL_TO_range_jet[i])
+        TOP_takeoff_jet[i,j] = T_W_calc(W_S_x[j],k,C_L_TO_range_jet[i])
 # the data
 l = np.linspace(0, 0.8, 200)
 x = np.linspace(0, 4000, 200)
