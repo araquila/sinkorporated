67d3d5047ceaf1119e22619818ea1f6c8119a08a
Rick Nelen <ricknelen@gmail.com>
2019-05-10 14:03:09 +0200

    Updated wing design

--- concept_design/liftingbody/class1sizing_liftingbody.py
+++ concept_design/liftingbody/class1sizing_liftingbody.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 #FUSELAGE SIZING
 def fuselage(n_passenger, n_crew, n_seats_abreast, n_aisles):
 
@@ -88,37 +89,58 @@ def fuselage(n_passenger, n_crew, n_seats_abreast, n_aisles):
     return length_nose, length_cabin, length_tail, length_fuselage, diameter_fuselage_outside, width_fuselage_outside
 
                     #WING SIZING
-def wing(Mach_cruise, S, A, C_L, high=False, mid=False, low=False):
-    #AERODYNAMICS
-    Mach_dd=Mach_cruise+0.03
-    Mach_t=0.935
-    #quarter chord sweep
-    if Mach_cruise <0.7:
-        sweep_chord_0_25=0
-    else :
-        sweep_chord_0_25=math.acos(0.75*(Mach_t/Mach_dd)) #[rad]
-    #geometric parameters
-    #taper ratio
-    taper=0.2*(2-sweep_chord_0_25)
-    #wingspan
-    b=math.sqrt(S*A)
-    #Chord lengths
-    rootchord=(2*S)/((1+taper)*b)
-    tipchord=taper*rootchord
-    #half wing sweep
-    sweep_chord_0_5=10*(math.pi/180)
-    #thickness-to-chord ratio
-    cos=math.cos(sweep_chord_0_5)
-    thickness_chord_ratio=((cos**3)*(Mach_t-Mach_dd*cos)-(0.115*C_L**1.5))/(cos**2)
-    if thickness_chord_ratio > 0.18:
-        thickness_chord_ratio=0.18
-    #dihedral angle
-    dihedral=3-(sweep_chord_0_5*(180/math.pi)/10)
+def det_quarter_chord_sweep(M_cruise, supercritical = False, delta_mach = 0.03):
+    """
+    determines the quarter chord sweep in radians
+    """
+    # Delta_mach can range from 0 to 0.05 but is given as 0.03 in ADSEE Slides
+    if 0.7 < M_cruise < 1:
+        if supercritical:
+            sweep = 0.75 * 0.935 / (M_cruise + delta_mach) # 0.935 from statistical data from Torenbeek
+            return np.arccos(sweep)
+        else:
+            sweep = 0.75 / (M_cruise + delta_mach)
+            return np.arccos(sweep)
+    if M_cruise <= 0:
+        raise NameError("Plane flying backwards??")
+    if M_cruise >= 1:
+        raise NameError("Going supersonic now, are we?")
+    else:
+        return np.arccos(1)
+
+def det_planform(S, AR, M_cruise, C_L_cruise, sweep, supercritical = False, delta_mach = 0.03):
+    # Delta_mach can range from 0 to 0.05 but is given as 0.03 in ADSEE Slides
+    b = np.sqrt(AR * S)
+    taper = 0.2 * (2 - sweep)
+    root_chord = (2 * S)/((1 + taper) * b)
+    tip_chord = taper * root_chord
+    half_chord_sweep = np.arctan(np.tan(sweep) - (4 / AR) * (0.25 * (1 - taper)/(1 + taper)))
+    if 0 < M_cruise < 1:
+        if supercritical:
+            t_c_ratio = min(0.18, (np.cos(half_chord_sweep)**3 * (0.935 - (M_cruise + delta_mach) * np.cos(half_chord_sweep)) - 0.115 * C_L_cruise**1.5) / np.cos(half_chord_sweep)**2)
+        else:
+            t_c_ratio = min(0.18, (np.cos(half_chord_sweep)**3 * (1 - (M_cruise + delta_mach) * np.cos(half_chord_sweep)) - 0.115 * C_L_cruise**1.5) / np.cos(half_chord_sweep)**2)
+    if M_cruise <= 0:
+        raise NameError("Plane flying backwards??")
+    if M_cruise >= 1:
+        raise NameError("Going supersonic now, are we?")
+    return b, taper, root_chord, tip_chord, t_c_ratio
+
+def det_dihedral_angle(sweep, high = False, mid = False, low = False):
+    dihedral = sweep * 18 / np.pi
     if high:
-        dihedral=dihedral-2 #0-1[deg]
+        angle = 1 - dihedral
+        return angle
+    if mid:
+        angle = 3 - dihedral
+        return angle
     if low:
-        dihedral=dihedral+2
-    return taper, b, rootchord, tipchord, sweep_chord_0_5, sweep_chord_0_25, thickness_chord_ratio, dihedral
+        angle = 5 - dihedral
+        return angle
+    else:
+        raise NameError("Where is the wing?")
+
+
 
                     #ENGINE DIMENSIONS
 def enginedimensions_tbp(rho0, n_engines, P_TO_tbp):
