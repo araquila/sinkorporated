a41030bcae44a1b359ba05d570cab94a936f5801
Matthijs Torsy <m.torsij@student.tudelft.nl>
2019-05-15 09:46:05 +0200

    merge conflict fixed

--- concept_design/conventional/class1sizing_conventional.py
+++ concept_design/conventional/class1sizing_conventional.py
@@ -147,60 +147,6 @@ def det_dihedral_angle(sweep, high = False, mid = False, low = False):
 
 def enginedimensions(rho_0,n_engines, P_TO_tbp, T_TO_jet, tbp=False, jet=True, jettypeB=False, jettypeC=False):
                     #TURBOPROP
-<<<<<<< HEAD
-if tbp:
-    #turboshaft dimensions
-    diameter_engine=0.2*(P_TO_tbp/(1000*n_engines))**0.18
-    length_engine=0.1*(P_TO_tbp/(1000*n_engines))**0.4
-    #propeller dimensions
-    diameter_propeller=0.55*(P_TO_tbp/(1000*n_engines))**0.25
-    #engine envelope dimensions
-    height_engine_envelope=1.5*diameter_engine
-    width_engine_envelope=1.1*diameter_engine
-    length_engine_envelope=length_engine
-    return diameter_engine, length_engine, diameter_propeller
-if jet:
-    bypass_ratio=8
-    a_0=340.3 #[m/s]
-    e_nozzle=0.97
-    e_tf=0.75
-    T_t4=1500 #1350-1650 [K]
-    G=(T_t4/600)-1.25
-
-    massflow=(T_TO_jet*(1+bypass_ratio))/(n_engines*a_0*math.sqrt(5*e_nozzle*G*(1+e_tf*bypass_ratio)))
-                #Intake dimensions
-    inlet_spinner_ratio=0.05*(1+(rho_0*a_0)/massflow+3*bypass_ratio/(1+bypass_ratio))
-    diameter_inlet=1.65*math.sqrt((massflow/(rho_0*a_0)+0.0050)/(1-(inlet_spinner_ratio)**2))
-    diameter_highlight=diameter_inlet
-                #Fan cowling dimensions
-    phi_fan_cowling=0.6 #0.5-0.75
-    if jettypeB:
-        c_l_nacelle=9.8
-        delta_l_nacelle=0.05
-        beta_nacelle=0.35
-    if jettypeC:
-        c_l_nacelle=7.8
-        delta_l_nacelle=0.10
-        beta_nacelle=0.21+(0.12/math.sqrt(phi_fan_cowling-0.3))
-    #nacelle length
-    length_nacelle=c_l_nacelle*(math.sqrt((massflow*(1+0.2*bypass_ratio))/(rho_0*a_0*(1+bypass_ratio)))+delta_l_nacelle)
-    #fan cowl length
-    length_fan_cowl=phi_fan_cowling*length_nacelle
-    #maximum nacelle diameter
-    diameter_nacelle=diameter_inlet+0.06*phi_fan_cowling*length_nacelle+0.03
-    #exit diameter fan
-    diameter_exit_fan=diameter_nacelle*(1-(phi_fan_cowling**2)/3)
-                #gas generator cowling dimensions
-    #exposed length of gas generator
-    length_generator=(1-phi_fan_cowling)*length_nacelle
-    #gas generator cowling at fan exit diameter
-    gasgenerator_coefficient=(massflow*bypass_ratio)/(rho_0*a_0)
-    diameter_gas_generato_fan=diameter_exit_fan*((0.089+4.5)/(0.067+5.8))**2
-    #gas generator cowling at gas generator exit diameter
-    diameter_gas_generator=0.55*diameter_gas_generato_fan
-
-    return length_nacelle, length_f, diameter_highlight, diameter_exit_fan, diameter_gas_generator
-=======
     if tbp:
         #turboshaft dimensions
         diameter_engine=0.2*(P_TO_tbp/(1000*n_engines))**0.18
@@ -253,7 +199,6 @@ if jet:
         diameter_gas_generator=0.55*diameter_gas_generato_fan
 
         return length_nacelle, length_fan_cowl, diameter_highlight, diameter_exit_fan, diameter_gas_generator, diameter_nacelle
->>>>>>> 018cc949cb2d19aa2f50d3e706909f93015b5b14
 
 def empennage(V_h, V_v, l_h, l_v, S, b, c):
     S_h = (V_h * S * c) / l_h
@@ -279,9 +224,6 @@ def empennage(V_h, V_v, l_h, l_v, S, b, c):
     tip_chord_h = root_chord_h * taper_h
     sweepLE_h = sweepqc_h + np.degrees(np.arctan((0.25*root_chord_h-0.25*tip_chord_h)/(span_h/2)))
 
-<<<<<<< HEAD
-    return S_h, span_h, root_chord_h, tip_chord_h, sweepqc_h, sweepLE_h, S_v, span_v, root_chord_v, tip_chord_v, sweepLE_v
-=======
     return AR_h, AR_v, S_h, span_h, root_chord_h, tip_chord_h, sweepqc_h, sweepLE_h, S_v, span_v, root_chord_v, tip_chord_v, sweepLE_v
 
 def undercarriage(main_landing_pos, nose_landing_pos, length_fuselage, length_tail, diameter_fuselage_outside):
@@ -317,4 +259,3 @@ def tiresizing(MTOW, LCN):
     # Nose wheel = 0.46 x 0.11 - 0.25
 
     return tire_pressure, P_mw, P_nw
->>>>>>> 018cc949cb2d19aa2f50d3e706909f93015b5b14
