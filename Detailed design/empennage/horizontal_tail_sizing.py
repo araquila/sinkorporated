49afb157fcd0d1a502606edfb09aa83caba51839
Bas <BasvManen@Gmail.com>
2019-05-29 09:21:06 +0200

    Added CLalpha AminH

--- Detailed design/empennage/horizontal_tail_sizing.py
+++ Detailed design/empennage/horizontal_tail_sizing.py
@@ -1,10 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+M_cruise = 0.6                  # Cruise Mach number [-]
+D_fuselage = 2.8                # Diameter of the fuselage [m]
+b = 31                          # Span of the aircraft [m]
+S = 50                          # Wing surface area [m^2]
+c_root = 1.8                    # Wing root chord [m]
+
 # Horizontal Tail Parameters
-A_h = 4 
-M_cruise = 0.6
-sweep_h = 15
+A_h = 4                         # Aspect ratio of the horizontal tail [-]
+sweep_h = 15                    # Sweep of the horizontal tail [deg]
+
+# Main Wing Parameters
+A_w = 20                        # Aspect ratio of the main wing [-]
+sweep_w = 0                     # Sweep of the main wing [deg]
 
 # General Aircraft Parameters
 x_ac = 10                       # Position of the aerodynamic center [m]
@@ -15,8 +24,7 @@ Vh_V2 = 1                       # Correction factor of velocity over the tail, s
 xLEMAC = 10                     # Position of the leading edge of the MAC [m]
 MAC = 1.8                       # Length of the MAC [m]
 
-C_L_alpha_Aminh = 5.35          # Lift curve of the entire aircraft without horizontal tail [rad^-1]
-C_L_h = -0.56                   # Lift coefficient of the horizontal tail [-]
+C_L_h = -0.35*(A_h)**(1/3)      # Lift coefficient of the horizontal tail, for a fixed tail [-]
 C_L_Aminh = 1.93                # Lift coefficient of the entire aircraft without horizontal tail [-]
 C_m_ac = -0.5                   # Moment coefficient around the aerodyamic center [-]
 downwash = 0.022                # Downwash effect on the horizontal tail [-]
@@ -27,7 +35,8 @@ SM = 0.05                       # Stability margin as a percentage of MAC [-]
 # DATCOM Estimation Methods
 beta = np.sqrt(1-M_cruise**2)
 C_L_alpha_h = (2*np.pi*A_h) / (2 + np.sqrt(4 + ((A_h*beta)/0.95)**2 * (1 + (np.tan(np.radians(sweep_h))/beta**2)))) 
-
+C_L_alpha_w = (2*np.pi*A_w) / (2 + np.sqrt(4 + ((A_w*beta)/0.95)**2 * (1 + (np.tan(np.radians(sweep_w))/beta**2))))
+C_L_alpha_Aminh = C_L_alpha_w * (1 + 2.15 * (D_fuselage / b)) * ((S - c_root * D_fuselage) / S) + (np.pi / 2) * (D_fuselage**2 / S)
 
 # Make array of tail surfaces
 Sh_S = np.linspace(0, 0.6, 100)
