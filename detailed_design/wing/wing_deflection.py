b848a61895d8b1d6f848b8b288b801b6aa903666
Robert <r.s.c.coenen@student.tudelft.nl>
2019-05-29 14:24:34 +0200

    update

--- detailed_design/wing/wing_deflection.py
+++ detailed_design/wing/wing_deflection.py
@@ -2,7 +2,7 @@
 """
 Created on Wed May 29 09:14:33 2019
 
-@author: rober
+@author: robert
 """
 
 import matplotlib.pyplot as plt
@@ -12,7 +12,7 @@ import parameters as p
 
 
 L = p.b/2
-F_sy = 10000
+F_sy = 1000
 safetyfactor = 1.1
 lift = p.MTOW*safetyfactor/2 #per wing
 
@@ -26,12 +26,16 @@ def defl(x):
 
     defl_F_strut = F_sy*p.x_strut**3/3 + F_sy*p.x_strut**2*(x-p.x_strut)/2
 
-    defl_W_wing = p.taper*p.W_wing*x**2*(6*L**2-4*L*x+x**2)/24 + (1-p.taper)*p.W_wing*x**2*(20*L**3-10*L**2*x+5*L*x**2-x**3)/(120*L)
+    defl_W_wing = p.taper*p.W_wing/L*x**2*(6*L**2-4*L*x+x**2)/24 + (1-p.taper)*p.W_wing/L*x**2*(20*L**3-10*L**2*x+5*L*x**2-x**3)/(120*L)
     
     defl_lift = lift*L/(3*np.pi**4)*(48*L**3*np.cos(np.pi*x/(2*L))-48*L**3+3*np.pi**3*L*x**2-np.pi**3*x**3)
     
     return -(defl_W_eng + defl_W_pod + defl_W_fuel + defl_F_strut + defl_W_wing - defl_lift)
+ #   return "engine",defl_W_eng,"pod",defl_W_pod,"fuel", defl_W_fuel ,"strut", defl_F_strut,"wing", defl_W_wing,"lift",defl_lift
+#def Izz(x):
+    
 
+print(defl(p.b/2))
 print(defl(p.b/2))
 x = np.linspace(0,p.b/2,100)
 plt.figure()
