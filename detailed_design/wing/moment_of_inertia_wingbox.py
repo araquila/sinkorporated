94b3863b760968fe8a5684d44da8ddf5d465aec0
Robert <r.s.c.coenen@student.tudelft.nl>
2019-06-04 16:21:36 +0200

    update

---
+++ detailed_design/wing/moment_of_inertia_wingbox.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 14:48:26 2019
+
+@author: robert
+"""
+
+import parameters as p
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+t_sheet = 0.003 #m
+t_hat = 0.001 #m
+t_z = 0.001 #m
+
+
+#hat geometry
+a = 0.06
+b = 0.04
+c = 0.05
+
+width_hat = 2*b+c-t_hat
+
+A_hat = t_hat*(2*b+2*a+c)
+
+y_centroid_hat = a + 2*t_hat - ( 2*b*t_hat*(a+3*t_hat/2) + 2*a*t_hat*(a/2+t_hat)+c*t_hat*t_hat/2 ) / (t_hat*(2*b+2*a+c))
+
+I_zz_hat = 2*(b*t_hat*(a+3*t_hat/2)**2 + a**3*t_hat/12 + a*t_hat*(a/2+t_hat)**2)
+
+
+#Z-stiffener geometry
+d = 0.04
+e = 0.06 
+f = 0.045
+
+width_z = d+f-t_z
+
+A_z = t_z*(d+e+f)
+
+y_centroid_z = (d*t_z**2/2 + e*t_z*(t_z+e/2)+f*t_z*(e+3*t_z/2))/(t_z*(d+e+f))
+
+I_zz_z = e**3*t_z*(e+t_z)**2 +f*t_z*(e+3*t_z/2)**2
+
+#section properties
+n_top = 0 # at the upper skin
+
+n_bottom = 0 # at the lower skin
+
+width_top = width_hat
+width_bottom = width_z
+
+y_top = y_centroid_hat
+y_bottom = y_centroid_z
+
+A_top = A_hat
+A_bottom = A_z
+
+
+#height at root of the wing box
+h_root = 0.929*p.h_max_root_wingbox
+h_tip = 0.825*p.h_max_tip_wingbox #arbitrary
+
+def width_wingbox(x):
+    return p.w_root_wingbox - (2/p.b) * x * (p.w_root_wingbox - p.w_tip_wingbox) 
+
+def height_wingbox(x):
+    return h_root - (2/p.b) * x * (h_root - h_tip)
+
+def y_neutral_line(x):
+    return (p.n_upper_skin*(height_wingbox(x)-p.h_stiffener/2)*p.A_stiffener + p.n_lower_skin*(p.h_stiffener/2)*p.A_stiffener) / ((p.n_upper_skin+p.n_lower_skin)*p.A_stiffener)
+
+#stiffener spacing
+top_spacing = (width_wingbox(0) - n_top * (width_top))/(n_top+1)
+
+lower_spacing = (width_wingbox(0) - n_bottom * (width_bottom))/(n_bottom+1)
+
+centroid_root = (2*((t_sheet+height_wingbox(0)/2)*(height_wingbox(0)-2*t_sheet)*t_sheet) + (-t_sheet/2+height_wingbox(0))*width_wingbox(0)*t_sheet + t_sheet**2/2*width_wingbox(0) + n_top*(3*t_sheet/2+height_wingbox(0)-y_top)*A_top + n_bottom*(t_sheet+y_bottom)*A_bottom) / (2*(height_wingbox(0)-2*t_sheet)*t_sheet + 2*width_wingbox(0)*t_sheet + n_top * A_top + n_bottom * A_bottom)
+
+#centroid_root = 2*((t_sheet + (height_wingbox(0)-2*t_sheet)/2)*(height_wingbox(0)-2*t_sheet)*t_sheet + (3*t_sheet/2 + height_wingbox(0)))/(2*(height_wingbox(0)-2*t_sheet)*t_sheet + 2*width_wingbox(0)*t_sheet)
+print(height_wingbox(0)/2)
+print(centroid_root)
+
+
+
+
+
+
