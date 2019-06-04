bad7ec987e7d93047f1c565e9e941369ebe8ccba
Bas <BasvManen@Gmail.com>
2019-06-04 14:02:57 +0200

    Parameters of the empennage changed

--- detailed_design/undercarriage/undercarriage_sizing.py
+++ detailed_design/undercarriage/undercarriage_sizing.py
@@ -1,13 +1,20 @@
 import parameters as p
 import numpy as np
 
-# Load on Nose Landing Gear
-load_nl = 0.08
-load_ml = 1- load_nl
+# Insert Scrap Angle Here
+scrap_angle = 15
 
+# Nose Landing Gear Parameters
+load_nl = 0.08
+l_nl = 2        # m
+
+# Centre of Gravity Calculation
 x_cg = 0.71 * p.MAC + 0.4283 * p.l_fuselage
 
-# Position of the Nose Landing Gear
-l_nl = 2        #m
 
+#Calculate Main Landing Gear Parameters
+load_ml = 1- load_nl
 l_ml = ((load_nl * (x_cg-l_nl)) / load_ml) + x_cg
+distance_to_tail = p.l_fuselage - p.l_tail - l_ml
+
+np.radians(scrap_angle)
