e0d58e6639b5597a6a567105d3beb1779d9bd34c
Rick Nelen <ricknelen@gmail.com>
2019-06-05 15:16:52 +0200

    changed aileron position

--- Sandboxes/Sandbox_Rick.py
+++ Sandboxes/Sandbox_Rick.py
@@ -5,13 +5,15 @@ import numpy as np
 angle = 30/180*np.pi
 
 # Maximum aileron deflection
-aileron_max = 25
+aileron_max = 20
 
+MTOW = 173185.74
+rho = 0.5258
+S = 50
 # Speed (cruise or landing? The latter is crucial)
-V = 50
-
+V = 49    # np.sqrt(MTOW/(0.5*2.1*rho * S ))
+print(V)
 # Geometry
-S = 49
 b = 31.3
 root_chord = 2.24
 taper = 0.4
@@ -20,7 +22,7 @@ tip_chord = root_chord * taper
 # Aerodynamics
 Cd0 = 0.008
 Cl_alpha = 2 * np.pi
-tau = 0.48
+tau = 0.57
 
 # Functions according to ADSEE
 def chord(y):
@@ -30,7 +32,7 @@ def fun1(y):
 def fun2(y):
     return y * y * chord(y)
 
-c1, err = quad(fun1,0.775*b/2,0.9*b/2)
+c1, err = quad(fun1,0.8*b/2,0.95*b/2)
 Cl_delta_aileron = 2 * Cl_alpha * tau / (S * b) * c1
 
 c2, err = quad(fun2,0,b/2)
@@ -41,4 +43,4 @@ P = - Cl_delta_aileron / Clp * aileron_max * (2 * V) / b
 # Required time to turn; this should be smaller than 1.5
 t = angle / P
 
-print(Cl_delta_aileron, Clp)
+print(t)
