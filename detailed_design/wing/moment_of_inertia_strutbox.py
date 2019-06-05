f6fc3977de2358401e430f6a947ce0d5c68ad141
Matthijs Torsy <m.torsij@student.tudelft.nl>
2019-06-05 09:37:44 +0200

    Moment of inertia for strutbox

--- detailed_design/wing/moment_of_inertia_strutbox.py
+++ detailed_design/wing/moment_of_inertia_strutbox.py
@@ -1,60 +1,133 @@
-import numpy as np
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 14:48:26 2019
+
+@author: robert
+"""
+
 import parameters as p
+import numpy as np
 import matplotlib.pyplot as plt
 
-#wingbox discretization
-discretizations = 26
-stepsize = p.b/2/discretizations
-safety_factor = p.safetyfactor_wingbox
+### STRUT DIAMETER ###
+A_strut_carbon = p.F_strut / p.ult_stress_carbon
+A_strut_al2014 = p.F_strut / p.ultimate_stress_al2014
 
 
-#spanwise locations for each of the discretizations
-steps = np.zeros(discretizations)
-for i in range(len(steps)):
-    steps[i] = (i+.5)*stepsize
+
+### STRUTBOX ###
+t_sheet = 0.003 #m
+t_hat = 0.002 #m
+t_z = 0.002 #m
 
 
-#height wing box
-h_root = 0.929*p.h_max_root_strutbox 
-h_tip = h_root
+#hat geometry
+a = 0.06
+b = 0.04
+c = 0.05
 
-def width_wingbox(x):
+width_hat = 2*b+c-t_hat
+
+A_hat = t_hat*(2*b+2*a+c)
+
+y_centroid_hat = a + 2*t_hat - ( 2*b*t_hat*(a+3*t_hat/2) + 2*a*t_hat*(a/2+t_hat)+c*t_hat*t_hat/2 ) / (t_hat*(2*b+2*a+c))
+
+I_zz_hat = 2*(b*t_hat*(a+3*t_hat/2)**2 + a**3*t_hat/12 + a*t_hat*(a/2+t_hat)**2)
+
+
+#Z-stiffener geometry
+d = 0.04
+e = 0.06 
+f = 0.045
+
+width_z = d+f-t_z
+
+A_z = t_z*(d+e+f)
+
+y_centroid_z = (d*t_z**2/2 + e*t_z*(t_z+e/2)+f*t_z*(e+3*t_z/2))/(t_z*(d+e+f))
+
+I_zz_z = e**3*t_z*(e+t_z)**2 +f*t_z*(e+3*t_z/2)**2
+
+#section properties
+n_top = 0 # at the upper skin
+n_bottom = 0 # at the lower skin
+
+width_top = width_hat
+width_bottom = width_z
+
+y_top = y_centroid_hat
+y_bottom = y_centroid_z
+
+A_top = A_hat
+A_bottom = A_z
+
+I_zz_top = I_zz_hat
+I_zz_bottom = I_zz_z
+
+#height at root of the wing box
+h_root = 0.929*p.h_max_root_strutbox
+h_tip = h_root #arbitrary
+
+def width_strutbox(x):
     return p.w_root_strutbox - (2/p.b) * x * (p.w_root_strutbox - p.w_tip_strutbox) 
 
-def height_wingbox(x):
+def height_strutbox(x):
     return h_root - (2/p.b) * x * (h_root - h_tip)
 
 def y_neutral_line(x):
-    return (p.n_upper_skin*(height_wingbox(x)-p.h_stiffener/2)*p.A_stiffener + p.n_lower_skin*(p.h_stiffener/2)*p.A_stiffener) / ((p.n_upper_skin+p.n_lower_skin)*p.A_stiffener)
+    return (p.n_upper_skin*(height_strutbox(x)-p.h_stiffener/2)*p.A_stiffener + p.n_lower_skin*(p.h_stiffener/2)*p.A_stiffener) / ((p.n_upper_skin+p.n_lower_skin)*p.A_stiffener)
+
+#stiffener spacing
+top_spacing = (width_strutbox(0) - n_top * (width_top))/(n_top+1)
+
+lower_spacing = (width_strutbox(0) - n_bottom * (width_bottom))/(n_bottom+1)
+
+centroid_root = (2*((t_sheet+height_strutbox(0)/2)*(height_strutbox(0)-2*t_sheet)*t_sheet) + (-t_sheet/2+height_strutbox(0))*width_strutbox(0)*t_sheet + t_sheet**2/2*width_strutbox(0) + n_top*(3*t_sheet/2+height_strutbox(0)-y_top)*A_top + n_bottom*(t_sheet+y_bottom)*A_bottom) / (2*(height_strutbox(0)-2*t_sheet)*t_sheet + 2*width_strutbox(0)*t_sheet + n_top * A_top + n_bottom * A_bottom)
+
+#moment of inertia
+dy_vertical_flange = np.abs(height_strutbox(0)/2 - centroid_root)
+dy_upper_flange = np.abs((height_strutbox(0)-t_sheet/2)-centroid_root)
+dy_bottom_flange = np.abs(centroid_root - t_sheet/2)
+
+dy_top_stiffener = np.abs(centroid_root - (height_strutbox(0)-t_sheet-y_top))
+dy_bottom_stiffener = np.abs(centroid_root - (t_sheet + y_bottom))
+
+Izz = t_sheet*(height_strutbox(0)-2*t_sheet)**3/12 + n_top*I_zz_top + n_bottom*I_zz_bottom + 2*(height_strutbox(0)-2*t_sheet)*t_sheet*dy_vertical_flange**2 + width_strutbox(0)*t_sheet*dy_upper_flange**2 + width_strutbox(0)*t_sheet*dy_bottom_flange**2 + n_top*A_top*dy_top_stiffener**2 + n_bottom*A_bottom*dy_bottom_stiffener**2
 
 
-#arbitrary loading diagrams
-def M(x):
-    return 100000 - 100000 * x /(p.b/2)
+def I_zz_strutbox(x,n_top,n_bottom):
+    width_top = width_hat
+    width_bottom = width_z
     
-def T(x):
-    return 300000 - 300000 * x /(p.b/2)
+    top_spacing = (width_strutbox(0) - n_top * (width_top))/(n_top+1)
 
-
-
-def enclosed_area(x):
-    return width_wingbox(x)*height_wingbox(x)
-
-def effective_airfoil_depth(x):
-    bending_eff_factor = (1./3.)*(1+(height_wingbox(x+stepsize)/height_wingbox(x-stepsize))**2+(height_wingbox(x-stepsize)/height_wingbox(x-stepsize))**2)
-    max_height = height_wingbox(x+stepsize)
-    return bending_eff_factor*max_height
+    lower_spacing = (width_strutbox(0) - n_bottom * (width_bottom))/(n_bottom+1)
     
-def Ix(x):
-    moment_contribution = M(x)/(width_wingbox(x)*effective_airfoil_depth(x)*p.ultimate_bending_stress_al2024)
-    torsion_contribution = T(x)/(2*enclosed_area(x)*p.ultimate_shear_stress_al2024)
-    return safety_factor*width_wingbox(x)*effective_airfoil_depth(x)**2 / 2 * (moment_contribution+torsion_contribution)
+    y_top = y_centroid_hat
+    y_bottom = y_centroid_z
+    
+    A_top = A_hat
+    A_bottom = A_z
+    
+    I_zz_top = I_zz_hat
+    I_zz_bottom = I_zz_z
+    
+    centroid_root = (2*((t_sheet+height_strutbox(x)/2)*(height_strutbox(x)-2*t_sheet)*t_sheet) + (-t_sheet/2+height_strutbox(x))*width_strutbox(x)*t_sheet + t_sheet**2/2*width_strutbox(x) + n_top*(3*t_sheet/2+height_strutbox(x)-y_top)*A_top + n_bottom*(t_sheet+y_bottom)*A_bottom) / (2*(height_strutbox(x)-2*t_sheet)*t_sheet + 2*width_strutbox(x)*t_sheet + n_top * A_top + n_bottom * A_bottom)
+    
+    #moment of inertia
+    dy_vertical_flange = np.abs(height_strutbox(x)/2 - centroid_root)
+    dy_upper_flange = np.abs((height_strutbox(x)-t_sheet/2)-centroid_root)
+    dy_bottom_flange = np.abs(centroid_root - t_sheet/2)
+    
+    dy_top_stiffener = np.abs(centroid_root - (height_strutbox(x)-t_sheet-y_top))
+    dy_bottom_stiffener = np.abs(centroid_root - (t_sheet + y_bottom))
+    
+    Izz = t_sheet*(height_strutbox(x)-2*t_sheet)**3/12 + n_top*I_zz_top + n_bottom*I_zz_bottom + 2*(height_strutbox(x)-2*t_sheet)*t_sheet*dy_vertical_flange**2 + width_strutbox(x)*t_sheet*dy_upper_flange**2 + width_strutbox(x)*t_sheet*dy_bottom_flange**2 + n_top*A_top*dy_top_stiffener**2 + n_bottom*A_bottom*dy_bottom_stiffener**2
+    
+    return Izz,top_spacing,lower_spacing
 
-def equivalent_sheet_thickness(x):
-    moment_contribution = safety_factor * M(x) /(width_wingbox(x)*effective_airfoil_depth(x)*p.ultimate_bending_stress_al2024)
-    torsion_contribution = safety_factor * T(x) / (2*enclosed_area(x)*p.ultimate_shear_stress_al2024)
-    return moment_contribution + torsion_contribution
 
+x = np.linspace(0,16,50)
+plt.plot(x,I_zz_strutbox(x,5,3)[0])
+plt.show()
 
-print(Ix(equivalent_sheet_thickness(steps[-1])))
-print(equivalent_sheet_thickness(steps[-1]))
