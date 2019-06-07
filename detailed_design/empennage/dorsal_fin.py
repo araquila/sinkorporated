eecac74af7151e7272914c57d7279346d4528e51
Benjamin Harrison <benjaminharrison1998@gmail.com>
2019-06-07 10:16:43 +0200

    dorsal fin

--- detailed_design/empennage/dorsal_fin.py
+++ detailed_design/empennage/dorsal_fin.py
@@ -1,9 +1,18 @@
 import numpy as np
 import parameters as p
+import empennage.empennage_sizing as emp
 
-S_df = 0.164 * p.S_v
-S_df_total = 0.19 * p.S_v
-sweep_0_dorsal = 2.244 * p.sweep_0_v
-root_chord_dorsal = 2.699 * p.root_chord_v
-length_dorsal = 1.156 * root_chord_dorsal
-height_dorsal = 3.664 * p.b_v
+S_df = 0.164 * emp.S_v
+S_df_total = 0.19 * emp.S_v
+delta_sweep = 70.49 - p.sweep_v
+sweep_0_dorsal = 2.244 * p.sweep_v
+c_root_dorsal = 2.699 * emp.c_root_v
+l_dorsal = 1.156 * c_root_dorsal
+h_dorsal = 3.664 * emp.b_v
+
+print("Dorsal fin surface area =", S_df)
+print("Dorsal fin total surface area =", S_df_total)
+print("Dorsal fin leading edge sweep =", sweep_0_dorsal)
+print("Dorsal fin root chord =", c_root_dorsal)
+print("Dorsal fin length =", l_dorsal)
+print("Dorsal fin height =", h_dorsal)
