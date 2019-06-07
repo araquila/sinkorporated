0f32631aa395dc618fafbc295dbadbacc1471222
Pieter <pieterbijl1@gmail.com>
2019-06-07 09:55:39 +0200

    added pressure

--- detailed_design/propulsion/untitled2.py
+++ detailed_design/propulsion/untitled2.py
@@ -1,7 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun  7 09:49:14 2019
+radius = 0.4 #[m]
+sigma_yield = 700e6 #[Pa]
+p = 12e6 #[Pa]
+safety_factor = 1.5 #[-]
 
-@author: Lousseaux
-"""
+def t_hoop(radius,sigma_yield,p):
+    t_hoop = (safety_factor*p*radius)/(2*sigma_yield)
+    return t_hoop
 
+def t_long(radius,sigma_yield,p):
+    t_long = (safety_factor*p*radius)/sigma_yield
+    return t_long
