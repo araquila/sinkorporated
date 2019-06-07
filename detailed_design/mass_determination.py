403b0fc3d5aa0bbbc87d33c7071de10bc8403a46
Bas <BasvManen@Gmail.com>
2019-06-07 13:58:22 +0200

    Added miscellaneous system weights

--- detailed_design/mass_determination.py
+++ detailed_design/mass_determination.py
@@ -6,10 +6,20 @@ import undercarriage.undercarriage_sizing as uc
 # Import class II mass estimation equations
 import class2 as c
 
-# Empennage weight estimation
+##### WEIGHT ESTIMATIONS #####
+
+# Empennage
 W_h_tail = c.det_hor_tail_weight(0.12*es.c_tip_v, es.b_h, p.mtom, es.S_h, p.l_h, es.sweep_h, es.A_h, 1.78)
 W_v_tail = c.det_ver_tail_weight(p.mtom, p.l_v, es.S_v, es.sweep_v, es.A_v, 0.12)
 
 # Landing Gear
 W_nose_landing = c.det_nose_lg_weight(p.mtom, uc.wheel_height, 2)
 W_main_landing = c.det_main_lg_weight(p.mtom, uc.wheel_height, 4, 2, p.V_stall)
+
+# Fuselage
+W_fuselage = c.det_fuselage_weight(p.mtom, p.l_cabin, p.S_wet_fuselage, p.taper, p.b, p.sweep_qc, p.LD_ratio)
+
+# Miscellaneous
+W_airco = c.det_aircond_weight(p.n_crew+p.n_pilots, p.volume_fuselage)
+W_anti_icing = c.det_anti_ice_weight(p.mtom)
+W_handling_gear = c.det_handling_gear_weight(p.mtom)
