2179c8d5818cd49274cb5fb79e42b7c5946469a5
Bas <BasvManen@Gmail.com>
2019-06-12 13:58:58 +0200

    Update to TO and climb

--- detailed_design/performance/climb.py
+++ detailed_design/performance/climb.py
@@ -20,16 +20,24 @@ T = p.T_TO
 P = p.P_TO
 W = p.MTOW
 
-alt = [0]
+alt = [8000, 9000, 13000]
+
+Vmin = []
+Vmax = []
+CLlist = []
+CDlist = []
+ROClist = []
 for altitude in alt:
+    # Calculate density at certain height
     temperature, pressure, rho, speed_of_sound = atmosphere_calc(altitude, t0, t_gradient, g, atR, atgamma)
     rho = 1.225 * rho
     pressure = 101325 * pressure
 
+    # Calculate stall speed
     CLmax = 2
-
     V_stall = np.sqrt((W/S)*(2/rho)*(1/CLmax))
 
+    # Determine Power Required and Power Available
     V = np.linspace(V_stall, V_stall+150, 1001)
     k1 = (1 / (np.pi * A * e))
     CL = W / (0.5*rho*V**2*S)
@@ -39,25 +47,35 @@ for altitude in alt:
     Pr = W * np.sqrt((W/S)*(2/rho)*(CD**2/CL**3))
     Pa = np.ones(len(V))*P
 
-    D0 = CD0 * 0.5 * rho * V**2 * S
-    DL = k1 * CL**2 * 0.5 * rho * V**2 * S
+    for i in range(len(Pr)):
+        if Pr[i] > Pa[i]:
+            index_Vmax = i
+            break
 
-    #plt.plot(V, D0)
-    #plt.plot(V, DL)
-    plt.plot(V, D*V)
-    plt.plot(V, Pa)
-plt.show()
+    for i in range(len(V)):
+        if V[i] > 0.6*speed_of_sound:
+            test = i
+            break
+    print(CL[test])
+    print(D[test])
+    
+    Vmin.append(V_stall)
+    Vmax.append(V[index_Vmax])    
+    CLlist.append(CL)
+    CDlist.append(CD)    
+    
+    ROC = (Pa-Pr)/W
+    ROC_max = np.max(ROC)
+    ROClist.append(ROC_max)
 
-
-
-ROC = (Pa-Pr)/W
-ROC_max = np.max(ROC)
-
-#plt.plot(V, Pr/1e6, label="Power Required")
-#plt.plot(V, Pa/1e6, label="Power Available")
-#plt.xlabel("Velocity [m/s]")
-#plt.ylabel("Power [MW]")
-#plt.legend()
+#plt.plot(ROClist, alt)
 #plt.show()
+#plt.plot(Vmin, alt)
+#plt.plot(Vmax, alt)
+#plt.show()
+    
+#plt.scatter(Vmin, alt)
+#plt.scatter(Vmax, alt)
 
-print("Maximum ROC:", np.round(ROC_max, decimals=2), "m/s")
+
+
--- detailed_design/performance/takeoff.py
+++ detailed_design/performance/takeoff.py
@@ -1,41 +1,61 @@
 import parameters as p
 import numpy as np
+import matplotlib.pyplot as plt
+
+from atmosphere import atmosphere_calc
 
 # Runway Input Parameters
 g = p.g
-rho = p.rho0
+rho0 = p.rho0
 mu = 0.02
 
-# Aircraft Input Parameters
-S = p.S
-CL = p.C_L_max_TO
-CD0 = p.Cd0
-CD = CD0 + CL**2 / (np.pi * p.A * p.e)
-T = p.T_TO
-W = p.MTOW
+altrange = np.linspace(0, 1500, 100)
+dist_TO = []
 
-# Take-Off Input Parameters
-V_stall = np.sqrt((W/S)*(2/rho)*(1/CL))
-gamma = 4 * (np.pi/180)
-n_rotation = 1.15
-h_screen = 15.2
+for altitude in altrange:
+    temperature, pressure, rho, speed_of_sound = atmosphere_calc(altitude, p.temperature0, p.temperature_gradient, p.g, p.R, p.gamma)
+    rho = 1.225 * rho
+    pressure = 101325 * pressure
+    
+    
+    # Aircraft Input Parameters
+    S = p.S
+    CL = p.C_L_max_TO
+    CD0 = p.Cd0
+    CD = CD0 + CL**2 / (np.pi * p.A * p.e)
+    T = p.T_TO
+    W = p.MTOW
+    
+    # Take-Off Input Parameters
+    V_stall = np.sqrt((W/S)*(2/rho)*(1/CL))
+    gamma = 4 * (np.pi/180)
+    n_rotation = 1.15
+    h_screen = 15.2
+    
+    V_min = V_stall
+    V_LOF = 1.05 * V_min
+    V_bar = V_LOF / (np.sqrt(2))
+    
+    # Calculate Forces
+    D = CD * 0.5 * rho * V_bar**2 * S
+    L = CL * 0.5 * rho * V_bar**2 * S
+    
+    # Calculate Acceleration and Rotation
+    a = (g/W) * (T - D - mu * (W - L))
+    R = (V_LOF**2) / ((n_rotation-1)*g)
+    
+    # Calculate Distances
+    s_ground = (V_LOF**2) / (2*a)
+    s_rotation = R * np.sin(gamma)
+    s_climb = (h_screen - R*(1-np.cos(gamma))) / np.tan(gamma)
+    s_TO = (s_ground + s_rotation + s_climb) * 1.15
+    
+    dist_TO.append(s_TO)
 
-V_min = V_stall
-V_LOF = 1.05 * V_min
-V_bar = V_LOF / (np.sqrt(2))
-
-# Calculate Forces
-D = CD * 0.5 * rho * V_bar**2 * S
-L = CL * 0.5 * rho * V_bar**2 * S
-
-# Calculate Acceleration and Rotation
-a = (g/W) * (T - D - mu * (W - L))
-R = (V_LOF**2) / ((n_rotation-1)*g)
-
-# Calculate Distances
-s_ground = (V_LOF**2) / (2*a)
-s_rotation = R * np.sin(gamma)
-s_climb = (h_screen - R*(1-np.cos(gamma))) / np.tan(gamma)
-s_TO = (s_ground + s_rotation + s_climb) * 1.15
-
-print("Take-Off Length Required:", np.round(s_TO, decimals=2), "m")
+    #print("Take-Off Length Required:", np.round(s_TO, decimals=2), "m")
+    
+plt.plot(altrange, dist_TO)
+plt.xlim([altrange[0],altrange[-1]])
+plt.ylim([dist_TO[0],dist_TO[-1]])
+plt.xlabel("Runway height above sea level [m]")
+plt.ylabel("Take-Off distance required [m]")
