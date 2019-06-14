8d1d6ee95545931048a3ad249d22dc5b8d53abaf
Bas <BasvManen@Gmail.com>
2019-06-14 11:22:00 +0200

    PLR save file

--- detailed_design/performance/payloadrange.py
+++ detailed_design/performance/payloadrange.py
@@ -1,5 +1,48 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+import parameters as p
+
+R = p.range_cruise
+
+MTOM = p.mtom
+MP = p.M_payload
+OEM = p.OEW / p.g
 
 
+# Point A
 
 
+RA = 1.
+PA = 1.
+
+# Point B
+
+
+RB = 1.
+PB = 1.
+
+# Point C
+
+
+RC = 1.
+PC = 1.
+
+# Point D
+
+
+RD = 1.
+PD = 1.
+
+
+rangelist = np.hstack([RA, RB, RC, RD]) / 1000.
+payloadlist = np.hstack([PA, PB, PC, PD])
+
+plt.plot(rangelist, payloadlist)
+
+plt.xlabel("Range [km]")
+plt.ylabel("Payload [kg]")
+plt.xlim([0, 4500])
+plt.ylim([0, 7000])
+
+plt.show()
