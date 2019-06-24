3a9408c4bf8647e8c5aeb52925e83717e6467570
Unknown <victor.vloeberghs@gmail.com>
2019-06-24 19:22:04 +0200

    y

--- detailed_design/finance/operational_cost_aquila.py
+++ detailed_design/finance/operational_cost_aquila.py
@@ -194,19 +194,19 @@ for i in range(len(mtomlist)):
     DOC = DOC_flt + DOC_maint + DOC_depr + DOC_lnr + DOC_fin
     LCC = (AEP + (DOC-DOC_depr) * R_blann * N_yr)/(1-F_disp)
     doclist.append(DOC)
-plt.clf()
-plt.scatter(mtomlist,doclist)
-plt.ylim([13.386,13.390])
-plt.show()
 #plt.clf()
-#labels = ['Insurance', 'Capital cost', 'Navigation cost', 'Landing fees','Flight crew','Maintenance','Fuel']
-#sizes = [round(C_ins*R_bl,2), round(DOC_fin*R_bl,2),round(C_nf*R_bl,2),round(C_lf*R_bl,2),round(C_crew*R_bl,2),round(DOC_maint*R_bl,2), round(C_pol*R_bl,2)]
-#patches, texts, pcts = plt.pie(sizes,labels=sizes, startangle=90,autopct='%1.1f%%',pctdistance=0.80, labeldistance=1.03)
-#plt.legend(patches, labels, loc='center left')
-#plt.axis('equal')
-#plt.tight_layout()
-#plt.title('Operational costs 1000 km Aquila, Total cost: '+str(round((DOC-DOC_depr)*R_bl,2))+' USD')
+#plt.scatter(mtomlist,doclist)
+#plt.ylim([13.386,13.390])
 #plt.show()
+plt.clf()
+labels = ['Insurance', 'Capital cost', 'Navigation cost', 'Landing fees','Flight crew','Maintenance','Fuel']
+sizes = [round(C_ins*R_bl,2), round(DOC_fin*R_bl,2),round(C_nf*R_bl,2),round(C_lf*R_bl,2),round(C_crew*R_bl,2),round(DOC_maint*R_bl,2), round(C_pol*R_bl,2)]
+patches, texts, pcts = plt.pie(sizes,labels=sizes, startangle=90,autopct='%1.1f%%',pctdistance=0.80, labeldistance=1.03)
+plt.legend(patches, labels, loc='center left')
+plt.axis('equal')
+plt.tight_layout()
+plt.title('Operational costs 1000 km Aquila, Total cost: '+str(round((DOC-DOC_depr)*R_bl,2))+' USD')
+plt.show()
 
 #labels = ['Flight cost', 'Maintenance cost', 'Depreciation cost', 'Landing fees and navigation fees','Financing costs']
 #sizes = [DOC_flt, DOC_maint, DOC_depr, DOC_lnr,DOC_fin]
